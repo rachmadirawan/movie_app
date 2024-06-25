ff0a7569d66ffd547d7cf6fab64975620cb29c95
Rachmad Irawan <irawandbl#gmail.com>
2024-06-25 19:51:17 +0700

    update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
-export default function Home(): JSX.Element {
+const Home = ({ navigation }: any) : JSX.Element => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.marginBottom20}>Movie Page</Text>
+            <Button 
+                title='Pergi ke MovieDetail'
+                onPress={() => navigation.navigate("MovieDetail")}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    marginBottom20 : {
+        marginBottom: 20,
+    },
+}
+)
+
+export default Home
